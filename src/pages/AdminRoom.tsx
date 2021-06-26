bf5fc51e34c250a815ff335bc3a2a91fd63e8e33
Williane <williane.tads@gmail.com>
2021-06-26 13:27:38 -0300

    quinta aula e deploy no firebase

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,59 +1,75 @@
-//import { FormEvent, useState } from 'react'
-import { useHistory, useParams } from "react-router";
-import { useRoom } from "../hooks/useRoom";
-import logoImg from "../assets/images/logo.svg";
+import { useHistory, useParams } from "react-router-dom";
 
+import logoImg from "../assets/images/logo.svg";
 import deleteImg from "../assets/images/delete.svg";
+import checkImg from "../assets/images/check.svg";
+import answerImg from "../assets/images/answer.svg";
 
 import { Button } from "../components/Button";
 import { Question } from "../components/Questions";
 import { RoomCode } from "../components/RoomCode";
-
-import "../styles/room.scss";
+// import { useAuth } from '../hooks/useAuth';
+import { useRoom } from "../hooks/useRoom";
 import { database } from "../services/firebase";
 
-//Record para tipar objetos, e dentro de <> fica o tipo da chave
+import "../styles/room.scss";
 
 type RoomParams = {
   id: string;
 };
 
 export function AdminRoom() {
+  // const { user } = useAuth();
   const history = useHistory();
   const params = useParams<RoomParams>();
   const roomId = params.id;
 
-  const { questions, title } = useRoom(roomId);
+  const { title, questions } = useRoom(roomId);
 
   async function handleEndRoom() {
-    database.ref(`rooms/${roomId}`).update({
+    await database.ref(`rooms/${roomId}`).update({
       endedAt: new Date(),
     });
+
     history.push("/");
   }
+
   async function handleDeleteQuestion(questionId: string) {
-    if (window.confirm("Tem certeza que deseja excluir essa pergunta?")) {
+    if (window.confirm("Tem certeza que você deseja excluir esta pergunta?")) {
       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
   }
 
   return (
     <div id="page-room">
       <header>
         <div className="content">
-          <img src={logoImg} alt="" />
+          <img src={logoImg} alt="Letmeask" />
           <div>
             <RoomCode code={roomId} />
             <Button isOutlined onClick={handleEndRoom}>
-              Encerrar sala{" "}
+              Encerrar sala
             </Button>
           </div>
         </div>
       </header>
-      <main className="content">
+
+      <main>
         <div className="room-title">
           <h1>Sala {title}</h1>
-          {questions.length > 0 && <span>{questions.length} perguntas</span>}
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
 
         <div className="question-list">
@@ -63,7 +79,28 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighlighted={question.isHighlighted}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img
+                        src={checkImg}
+                        alt="Marcar pergunta como respondida"
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque à pergunta" />
+                    </button>
+                  </>
+                )}
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
